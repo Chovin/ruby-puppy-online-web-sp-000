80c5517d1dd2c81d650319117c403c9c06b0a518
Chovin Carlson <Chovin@users.noreply.github.com>
2019-01-06 12:23:41 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all < self
+  end
+  
+  def clear_all
+    @@all = []
+  end
+  
+  def all
+    @@all
+  end
+  
+  
+end
